20e13680b95d505bd4aa4759eb932ccd07835b3f
Onyeke Adams Mike <onyekegodstime@gmail.com>
2024-09-06 16:44:18 +0100

    to-do-list application code

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function toDoList(){
+    const addButton= document.getElementById('add-task-btn')
+    const taskInput = document.getElementById('task-input')
+    const taskList = document.getElementById('task-list')
+
+    function addTask(){
+        const taskText= taskInput.value.trim()
+        if (taskText===''){
+            alert('Please enter a task')
+        }else{
+            const li = document.createElement('li')
+            li.textContent= taskText
+
+            const button = document.createElement('button')
+            button.textContent= "Remove"
+            button.className='remove-btn'
+            
+            button.onclick = function () {
+                li.remove()
+            }
+
+            li.appendChild(button)
+            taskList.appendChild(li)
+
+            taskInput.value=''
+        }
+
+    }
+    addButton.addEventListener('click', addTask)
+    taskInput.addEventListener('keypress', function(event){
+        if (event.key==='Enter'){
+            addTask()
+        }
+    })
+    document.addEventListener('DOMContentLoaded', addTask)
+})
